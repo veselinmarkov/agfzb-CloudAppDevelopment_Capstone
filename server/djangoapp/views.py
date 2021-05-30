810b1e441a9e1cbedc7c95cb143303a9485a02f9
veselinmarkov <veselin.markov@gmail.com>
2021-05-30 10:00:36 +0000

    Registration fixed

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
+# from .models import 
 # from .restapis import related methods
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 import logging
 import json
 
@@ -18,16 +19,12 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 # Create an `about` view to render a static about page
-# def about(request):
-#class StaticAboutUs(TemplateView):
-#    template_name = 'static_aboutus.html'
 def about(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/static_aboutus.html', context)
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
 def contact(request):
     context = {}
     if request.method == "GET":
@@ -39,9 +36,9 @@ def login_request(request):
     if request.method == 'POST':
         username = request.POST['username']
         psw = request.POST['psw']
-        print(username, psw)
+        # print(username, psw)
         user = authenticate(username=username, password=psw)
-        print(user)
+        # print(user)
         if user is not None:
             login(request, user)
             return redirect('djangoapp:index')
@@ -62,6 +59,22 @@ def registration_request(request):
     context = {}
     if request.method == 'GET':
         return render(request, 'djangoapp/registration.html', context)
+    else:
+        username = request.POST['username']
+        user_ok = False
+        try:
+            user = User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            user_ok = True
+        if user_ok:
+            user = User.objects.create_user(first_name=request.POST['firstname'], last_name=request.POST['lastname'],
+                username=username, password=request.POST['psw'])
+            login(request, user)
+            logger.error('New user created: ' +username)
+            return redirect('djangoapp:index')
+        else:
+            context['message'] = 'The user {} already exists'.format(username)
+            return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
