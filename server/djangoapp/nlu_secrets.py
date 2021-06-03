ffd6d47b8768dce0eacad08523d30aad728e5aaa
Veselin Markov <48570558+veselinmarkov@users.noreply.github.com>
2021-06-03 22:06:20 +0200

    Delete nlu_secrets.py

--- server/djangoapp/nlu_secrets.py
+++
@@ -1,3 +0,0 @@
-params = {}
-params['api_key'] = 'irT9-k1Lkj2TTY7N19up_FtDOUKtBTx-Ma0SQ2SFS3Cm'
-params['NLU_url'] = 'https://api.eu-gb.natural-language-understanding.watson.cloud.ibm.com/instances/b382fa57-d616-461e-a69b-d174a86e5946/v1/analyze?version=2020-08-01'
